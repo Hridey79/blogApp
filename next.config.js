00526aa649a9044e7ae3785708312ecd911ee1ba
HrideyVerma79 <82920883+HrideyVerma79@users.noreply.github.com>
2024-11-19 21:10:20 +0530

    next config changes

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
         hostname: "images.pexels.com"
       }
     ]
+  },
+  env: {
+    BASE_URL: "https://blog-app-five-delta-85.vercel.app/", // Add your domain here
   }
 }
 
